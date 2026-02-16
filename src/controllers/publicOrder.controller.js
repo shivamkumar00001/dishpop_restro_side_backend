0801bef7e979e8c123b9f3acbc3cafbfd573e5f6
shivamkumar00001 <shivamkumar933401@gmail.com>
2026-02-17 01:07:21 +0530

    added the online order part

--- src/controllers/publicOrder.controller.js
+++ src/controllers/publicOrder.controller.js
@@ -1,15 +1,38 @@
 const Customer = require("../models/customers.js");
 const asyncHandler = require("../middlewares/asyncHandler.js");
 
+// exports.getPublicOrdersByRestaurant = asyncHandler(async (req, res) => {
+//   const { username } = req.params;
+
+//   // Only expose SAFE fields
+//   const orders = await Customer.find({ username })
+//     .sort({ createdAt: 1 }) // order of ordering
+//     // .select(
+//     //   "status tableNumber createdAt items.name items.qty"
+//     // )
+//     .select(
+//   "orderType status tableNumber createdAt items.name items.qty delivery"
+// )
+
+//     .lean();
+
+//   res.status(200).json({
+//     success: true,
+//     count: orders.length,
+//     data: orders,
+//   });
+// });
+
 exports.getPublicOrdersByRestaurant = asyncHandler(async (req, res) => {
   const { username } = req.params;
 
-  // Only expose SAFE fields
   const orders = await Customer.find({ username })
-    .sort({ createdAt: 1 }) // order of ordering
-    .select(
-      "status tableNumber createdAt items.name items.qty"
-    )
+    .sort({ createdAt: 1 })
+    // .select(
+    //   "orderType status tableNumber createdAt items.name items.qty delivery"
+    // )
+    .select("status tableNumber orderType phoneNumber delivery createdAt items.name items.qty")
+
     .lean();
 
   res.status(200).json({
